dd55f10805e70a6b9cb7484213fb64721960c728
Attila István NAGY <me@nagyattilaistvan.hu>
2012-12-20 13:15:34 +0100

    Some structures and pseudocode of paging
    Also some whitespace correction and an error message displayed when the
    Multiboot magic number is invalid.

--- kernel.c
+++ kernel.c
@@ -1,5 +1,46 @@
 #include <stdint.h>
 
+#define NULL (void *) 0x0
+
+typedef unsigned int int32;
+
+typedef struct {
+    int32 source_start;
+    int32 source_end;
+    int32 target_start;
+} mem_mapping;
+
+typedef int32 __attribute__((aligned(16))) page_table[1024];
+
+int current_pagetable_set = 0;
+page_table page_directory[2];
+page_table page_tables[1024][2];
+
+void build_pagetables(int32 mapping_count, mem_mapping *map,
+                      int identity_mapping)
+{
+    // TODO use kmalloc when it is done
+    mem_mapping *curr_mapping;
+
+    current_pagetable_set = !current_pagetable_set;
+
+    // TODO zero out page_directory and page_tables
+    // TODO setting up page_directory does not depend on the mapping, so it
+    //      can be done here
+    if (identity_mapping)
+    {
+        // if identity mapping is set, initialize tables for identity mapping,
+        // then add the requested modifications here
+    }
+
+    for (curr_mapping = map; curr_mapping < (map + mapping_count); map++)
+    {
+        //unsigned int curr_source_start = curr_mapping->source_start;
+        // TODO check start alignment
+        // TODO find appropriate first page_table and the correct entry in it
+    }
+}
+
 void kputch(unsigned char ch)
 {
     volatile unsigned char *videoram = (unsigned char *)0xB8000;
@@ -7,19 +48,21 @@ void kputch(unsigned char ch)
     videoram[0] = ch;
     videoram[1] = 0x07;
 }
- 
+
 void kmain(void)
 {
     extern uint32_t magic;
  
     // The multiboot header
     //extern void *mbd;
- 
+
     if ( magic != 0x2BADB002 )
     {
         /* Something went not according to specs. Print an error */
         /* message and halt, but do *not* rely on the multiboot */
         /* data structure. */
+        kputs("Multiboot magic number mismatch! Freezing.");
+        while (1);
     }
  
    /* You could either use multiboot.h */
